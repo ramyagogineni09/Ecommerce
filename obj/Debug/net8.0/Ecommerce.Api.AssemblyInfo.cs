d45cb9ea65a0f1bd57e98d972db19d26d177162a
ramyagogineni09 <ramyagogineni08@gmail.com>
2026-01-05 13:10:28 -0600

    Added core domain entityes product and catagory

--- obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e27f2bd4654fd7529edc1a028f16e98d44f373")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
