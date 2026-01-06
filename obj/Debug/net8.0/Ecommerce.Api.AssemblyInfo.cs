6237138d58ed86ab0f7382707bc143c3d7c08904
ramyagogineni09 <ramyagogineni08@gmail.com>
2026-01-05 19:07:27 -0600

    Intial database migration for product and category

--- obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45cb9ea65a0f1bd57e98d972db19d26d177162a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567bbee4200f99c714c9635525f39d3dc45fbeac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
