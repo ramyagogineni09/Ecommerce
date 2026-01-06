567bbee4200f99c714c9635525f39d3dc45fbeac
ramyagogineni09 <ramyagogineni08@gmail.com>
2026-01-05 18:07:37 -0600

    Adedd EF core DbContext and SQL Server Configaration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+
+using Ecommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EcommerceDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e27f2bd4654fd7529edc1a028f16e98d44f373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45cb9ea65a0f1bd57e98d972db19d26d177162a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
